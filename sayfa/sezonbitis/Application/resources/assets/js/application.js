fd7ef03214264a4d8fb4c7e353fb3f51717d72e7
rchesapla <fatihcelikofficialtr@gmail.com>
2024-09-30 02:47:52 +0300

    +

--- sayfa/sezonbitis/Application/resources/assets/js/application.js
+++
@@ -1,7 +0,0 @@
-require('./global')
-require('./main');
-require('./ajax')
-require('./handler');
-require('./dashboard');
-require('./image-preview');
-require('./pdf-preview');
--- sayfa/sezonbitis/Application/resources/views/backend/includes/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p class="mb-0"> &copy; <span data-year></span> {{ $settings['website_name'] }} - -
-        {{ __('All rights reserved.') }} | <span class="capitalize">{{ config('vironeer.item.alias') }}</span>
-        v{{ config('vironeer.item.version') }}</p>
-    <p class="mb-0 ms-auto">{{ __('Powered by Vironeer') }}</p>
-</footer>
