327c98a37b38839f6723a500de8c94adce197772
rchesapla <fatihcelikofficialtr@gmail.com>
2024-12-23 04:05:59 +0300

    +

--- sayfa/v5/js/index.js
+++ sayfa/v5/js/index.js
@@ -1,3 +1,6 @@
+import { DOMElements } from '/modules/DOMElements.js'
+import { Calculator } from '/modules/calculator/index.js'
+
 const app = new Calculator({
     page: document.body.id,
     DOMElements
