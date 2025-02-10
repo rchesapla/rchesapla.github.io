4e17f2933eebb3538201b31ccddfb5fde722018e
rchesapla <fatihcelikofficialtr@gmail.com>
2025-02-11 00:38:41 +0300

    +

--- js/menüşablonu.js
+++
@@ -1,5 +0,0 @@
-﻿(function(){
-  var visitors= document.querySelector(".visitors-text");
-  const CurrentVisitors = 56+Math.floor(Math.random() * Math.floor(10));
-  visitors.innerHTML = "Şu anda <b>"+CurrentVisitors+ "</b> kişilik ziyaretçimiz bulunmakta!";
-})();
