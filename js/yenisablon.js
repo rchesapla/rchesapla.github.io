e97241a981842879096e1e8beea9cd047906cc59
rchesapla <fatihcelikofficialtr@gmail.com>
2025-02-26 05:01:59 +0300

    +

--- js/yenisablon.js
+++ js/yenisablon.js
@@ -1,81 +1,98 @@
-//Global variables
-var element;
+/* Toggle Style Switcher */
 
+const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
+styleSwitcherToggle.addEventListener('click', () => { document.querySelector('.style-switcher').classList.toggle('open'); })
 
-//Custom cursor
-(() => { 
+window.addEventListener('scroll', () => { if(document.querySelector('.style-switcher').classList.contains('open')) { document.querySelector('.style-switcher').classList.remove('open'); } })
 
-   const cursor = document.querySelector('.cursor');
-
-   document.addEventListener('mousemove', e => {
-      cursor.setAttribute('style', `top:  ${e.pageY - 25}px; left: ${e.pageX - 25}px;`);
-   });
-
-   document.addEventListener('click', () => { 
-      console.log("%c Click...!!!", "font-size: 20px; color:mediumspringgreen;");
-
-      cursor.classList.add('cursor--expand');
-      console.log(cursor.classList);
-   
-      setTimeout(() => {
-         cursor.classList.remove('cursor--expand');
-      }, 500);
-   });
-})();
-
-
-
-
-
-
-
-//Detect onclick event
-if (window.matchMedia("(max-width: 920px)").matches === false) {
-    $(".ham").on("click", function(){
-        $("menu").css("right", "0px");
-        $(".overlay").css("opacity","1");
-        $(".overlay").css("z-index","99");
-    });
-
-    $(".close").on("click", function(){
-        $("menu").css("right", "-400px");
-        $(".overlay").css("opacity","0");
-        $(".overlay").css("z-index","-1");
-    });
-
-    $(".overlay").on("click", function(){
-        $("menu").css("right", "-400px");
-        $(".overlay").css("opacity","0");
-        $(".overlay").css("z-index","-1");
-    });
-} else {
-    $(".ham").on("click", function(){
-        $(".nav_content").css("right", "0px");
-        $(".overlay").css("opacity","1");
-        $(".overlay").css("z-index","1");
-    });
-    
-    $(".close").on("click", function(){
-        $(".nav_content").css("right", "-200px");
-        $(".overlay").css("opacity","0");
-        $(".overlay").css("z-index","-1");
-    });
-    
-    $(".overlay").on("click", function(){
-        $(".nav_content").css("right", "-200px");
-        $(".overlay").css("opacity","0");
-        $(".overlay").css("z-index","-1");
-    });
+const alternateStyles = document.querySelectorAll('.alternate-style');
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute('title')) { style.removeAttribute('disabled'); } else { style.setAttribute('disabled', 'true'); }
+    })
 }
 
+/* Dark/Light Mode */
 
-//Scroller Nav
-window.onscroll = function() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("nav").css("background-color","var(--black)") ;
-    } else {
-        $("nav").css("background-color","transparent") ;
+const dayNight = document.querySelector('.day-night');
+dayNight.addEventListener('click', () => {
+    dayNight.querySelector('i').classList.toggle('fa-sun');
+    dayNight.querySelector('i').classList.toggle('fa-moon');
+    document.body.classList.toggle('dark');
+})
+
+window.addEventListener('load', () => {
+    if(document.body.classList.contains('dark')) { dayNight.querySelector('i').classList.add('fa-sun'); } else { dayNight.querySelector('i').classList.add('fa-moon'); }
+})
+
+/* Typing Animation */
+
+var typed = new Typed('.typing', { strings: ["", "Web Designer", "Web Developer", "Graphic Designer", "Youtuber"], typeSpeed: 100, Backspeed: 60, loop: true })
+
+/* Changing Aside Active Link */
+
+const nav = document.querySelector('.nav');
+const navList = nav.querySelectorAll('li');
+const totalNavList = navList.length;
+const allSection = document.querySelectorAll('.section');
+const totalSection = allSection.length;
+
+for(let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector('a');
+    a.addEventListener('click', function(){
+            removeBackSection();
+            for(let j = 0; j < totalNavList; j++) { 
+                if(navList[j].querySelector('a').classList.contains('active')) { addBackSection(j);/*allSection[j].classList.add('back-section');*/ }
+                navList[j].querySelector('a').classList.remove('active'); }
+        this.classList.add('active');
+        showSection(this);
+
+        if(window.innerWidth < 1200) { asideSectionTogglerBtn(); }
+    })
+}
+
+function addBackSection(num) { allSection[num].classList.add('back-section'); }
+
+function removeBackSection(){
+    for( let i = 0; i < totalSection; i++){ allSection[i].classList.remove('back-section'); }
+}
+
+function showSection(element){
+    for( let i = 0; i < totalSection; i++){ allSection[i].classList.remove('active'); }
+
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector('#' + target).classList.add('active');
+}
+
+function updateNav(element){
+    for(let i = 0; i < totalNavList; i++){
+        navList[i].querySelector('a').classList.remove('active');
+        const target = element.getAttribute('href').split('#')[1];
+        if(target === navList[i].querySelector('a').getAttribute('href').split('#')[1]) { navList[i].querySelector('a').classList.add('active'); }
+        
     }
+}
+
+document.querySelector('.hire-me').addEventListener('click', function(){
+    const sectionIndex = this.getAttribute('data-section-index');
+    /*console.log(sectionIndex);*/
+    showSection(this);
+    updateNav(this);
+    removeBackSection();
+    addBackSection(sectionIndex);
+})
+
+/* Activating Mobile Menu */
+
+const navTogglerBtn = document.querySelector('.nav-toggler');
+const aside = document.querySelector('.aside');
+
+navTogglerBtn.addEventListener('click', () => { asideSectionTogglerBtn(); })
+
+function asideSectionTogglerBtn(){
+    aside.classList.toggle('open');
+    navTogglerBtn.classList.toggle('open');
+    for(let i = 0; i < totalSection; i++) { allSection[i].classList.toggle('open'); }
 }
 
 
