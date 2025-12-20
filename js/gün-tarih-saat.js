b4f9d78095424c0b49d666102ce8bc41772533f8
rchesapla <fatihcelikofficialtr@gmail.com>
2025-12-20 17:05:42 +0300

    +

--- js/gün-tarih-saat.js
+++ js/gün-tarih-saat.js
@@ -40,7 +40,7 @@ princewedding.oncountup=function(result){
     //result is an object containing the current count up date/time, updated every second
     //Available properties: result["days"], result["hours"], result["minutes"], and result["seconds"]
     var mycountainer=document.getElementById("cpcontainer")
-    mycountainer.innerHTML="Çalışma süresi: <a style='color:red'>"+result['days']+"</a> gündür aktif."
+    mycountainer.innerHTML="Çevrimiçi: <a style='color:red'>"+result['days']+"</a> gün."
 }
 
 // TARİH AYARI //
