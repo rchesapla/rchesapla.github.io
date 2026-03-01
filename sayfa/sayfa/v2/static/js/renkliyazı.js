d09cdcaf092535aefbde8dd1a11a9ab3046f369b
rchesapla <rchesapla@gmail.com>
2026-03-01 19:59:21 +0300

    +

--- sayfa/sayfa/v2/static/js/renkliyazı.js
+++
@@ -1,7 +0,0 @@
-$('.name').on('mousemove',function(e){
-			centerX = $(this).width()/2;
-			var moveY = centerX - e.offsetX;
-			$(this).css({
-				'transform': 'translate(-50%,-50%) perspective(1500px) rotateY('+ moveY/20 +'deg)'
-			})
-		})
