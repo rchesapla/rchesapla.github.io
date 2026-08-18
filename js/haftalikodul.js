86da6380bf8e4a47c36e8676c37705467233256c
fatihcelikofficial <rchesapla@gmail.com>
2026-08-18 12:46:52 +0300

    +

---
+++ js/haftalikodul.js
@@ -0,0 +1,8 @@
+        var typed2 = new Typed('#haftalıködüller', {
+            strings: ["BTC: 40000 SAT", "ETH: 0.007","BNB: 0.044","MATIC: 5.5","DOGE: 140","SOL: 0.23"], // ☛ Güncellemeyi bekleyiniz.. ☚
+            typeSpeed: 70,
+            backSpeed: 30,
+            backDelay: 10000,
+            fadeOut: true,
+            loop: true
+        })
