956676c05b55a7ad55d963529df2663006ada63a
fatihcelikofficial <rchesapla@gmail.com>
2026-08-23 09:23:05 +0300

    +

---
+++ v3/js/modules/rollercoin.js
@@ -0,0 +1,51 @@
+export const blockReward = {
+    "BTC": {
+        dailyReward: 30000,
+        timePerBlock: 600,
+        name: "Bitcoin",
+        ticker: "BTC",
+        icon: "btc.svg"
+    },
+    "DOGE": {
+        dailyReward: 30,
+        timePerBlock: 600,
+        name: "Dogecoin",
+        ticker: "DOGE",
+        icon: "doge.svg"
+    },
+    "ETH": {
+        dailyReward: 0.008,
+        timePerBlock: 600,
+        name: "Ethereum",
+        ticker: "ETH",
+        icon: "eth.svg"
+    },
+    "BNB": {
+        dailyReward: 0.012,
+        timePerBlock: 600,
+        name: "Binancecoin",
+        ticker: "BNB",
+        icon: "bnb.svg"
+    },
+    "MATIC": {
+        dailyReward: 3,
+        timePerBlock: 600,
+        name: "Polygon",
+        ticker: "matic-network",
+        icon: "matic.svg"
+    },
+    "RLT": {
+        dailyReward: 30,
+        timePerBlock: 600,
+        name: "Rollertoken",
+        ticker: "RLT",
+        icon: "rlt.svg"
+    },
+    "SOL": {
+        dailyReward: 0.09,
+        timePerBlock: 600,
+        name: "Solana",
+        ticker: "SOL",
+        icon: "sol.svg"
+    }
+}
